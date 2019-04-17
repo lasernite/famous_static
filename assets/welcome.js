7e536c51a97429bd9bdd0bcafa0b874a2a64883f
Laser Nite <lasernite@gmail.com>
2019-04-17 15:53:54 -0700

    github pages

---
+++ assets/welcome.js
@@ -0,0 +1,10 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+window.onload = function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-112779023-3');
+}
